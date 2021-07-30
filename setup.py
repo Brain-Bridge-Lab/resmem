edeac1539d532e81732b7812135b9cef8b707c66
soybison <coen@needell.co>
2021-07-30 15:12:23 -0500

    synced with pypi, ready for publishing new readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="resmem",
-    version="1.1.2",
+    version="1.1.3",
     author="Coën D. Needell",
     author_email="coen@needell.co",
     description="A package that wraps the ResMem pytorch model.",
